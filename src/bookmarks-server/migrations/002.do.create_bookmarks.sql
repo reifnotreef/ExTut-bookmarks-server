f95d0f6946a4543372687859379f618b24992466
Bridger Reif Hammond <bridgerrhammond@gmail.com>
2019-11-10 12:52:24 -0600

    sync

--- src/bookmarks-server/migrations/002.do.create_bookmarks.sql
+++ src/bookmarks-server/migrations/002.do.create_bookmarks.sql
@@ -1,47 +1,45 @@
-CREATE TABLE "ingredients" (
-  "id" int NOT NULL GENERATED ALWAYS AS IDENTITY,
+DROP TABLE IF EXISTS "ingredients";
+
+DROP TABLE IF EXISTS "hardware";
+
+DROP TABLE IF EXISTS "instructions";
+
+DROP TABLE IF EXISTS "recipes";
+
+DROP TABLE IF EXISTS "recipe_ingredients";
+
+DROP TABLE IF EXISTS "recipe_hardware";
+
+CREATE TABLE IF NOT EXISTS "ingredients" (
+  "id" serial primary key,
   "name" varchar(50) NOT NULL
 );
 
-CREATE UNIQUE INDEX "PK_ingredients" ON "ingredients" ("id");
-
-CREATE TABLE "hardware" (
-  "id" int NOT NULL GENERATED ALWAYS AS IDENTITY,
+CREATE TABLE IF NOT EXISTS "hardware" (
+  "id" serial primary key,
   "name" varchar(50) NOT NULL
 );
 
-CREATE UNIQUE INDEX "PK_hardware" ON "hardware" ("id");
-
-CREATE TABLE "recipes" (
-  "id" int NOT NULL GENERATED ALWAYS AS IDENTITY,
+CREATE TABLE IF NOT EXISTS "recipes" (
+  "id" serial primary key,
   "name" varchar(50) NOT NULL,
   "prep_time" int NOT NULL,
   "cook_time" int NOT NULL,
   "category" varchar(50) NOT NULL,
-  "simplicity" int NOT NULL,
-  (
-    CHECK simplicity BETWEEN 1
-    and 5
-  )
+  "simplicity" int NOT NULL
 );
 
-CREATE UNIQUE INDEX "PK_recipes" ON "recipes" ("id");
-
-CREATE TABLE "instructions" (
-  "id" int NOT NULL GENERATED ALWAYS AS IDENTITY,
-  "recipe_id" int NOT NULL,
+CREATE TABLE IF NOT EXISTS "instructions" (
+  "id" serial primary key,
+  "recipe_id" serial NOT NULL,
   "step_number" int NOT NULL,
   "instructions" varchar(500) NOT NULL,
   CONSTRAINT "FK_72" FOREIGN KEY ("recipe_id") REFERENCES "recipes" ("id")
 );
 
-CREATE UNIQUE INDEX "PK_instructions" ON "instructions" ("id");
-
-CREATE INDEX "fkIdx_72" ON "instructions" ("recipe_id");
-
-CREATE TABLE "recipe_ingredients" (
-  "recipe_id" int NOT NULL,
-  "ingredient_id" int NOT NULL,
+CREATE TABLE IF NOT EXISTS "recipe_ingredients" (
+  "recipe_id" serial NOT NULL,
+  "ingredient_id" serial NOT NULL,
   "ingredient_amount" int NOT NULL,
   CONSTRAINT "FK_50" FOREIGN KEY ("recipe_id") REFERENCES "recipes" ("id"),
   CONSTRAINT "FK_56" FOREIGN KEY ("ingredient_id") REFERENCES "ingredients" ("id")
@@ -49,11 +47,13 @@ CREATE TABLE "recipe_ingredients" (
 
 CREATE UNIQUE INDEX "PK_recipe_reqs" ON "recipe_ingredients" ("ingredient_id", "recipe_id");
 
-CREATE INDEX "fkIdx_56" ON "recipe_ingredients" ("ingredient_id");
+CREATE INDEX "kdIdx_90" on recipe_ingredients ("recipe_id");
 
-CREATE TABLE "recipe_hardware" (
-  "recipe_id" int NOT NULL,
-  "hardware_id" int NOT NULL,
+CREATE INDEX "kdIdx_91" on recipe_ingredients ("ingredient_id");
+
+CREATE TABLE IF NOT EXISTS "recipe_hardware" (
+  "recipe_id" serial NOT NULL,
+  "hardware_id" serial NOT NULL,
   CONSTRAINT "FK_76" FOREIGN KEY ("hardware_id") REFERENCES "hardware" ("id"),
   CONSTRAINT "FK_79" FOREIGN KEY ("recipe_id") REFERENCES "recipes" ("id")
 );
